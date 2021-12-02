4f9f9961445b6b3e189232a9b4aa0095f97ffdec
Flück Christof <flucc1@bfh.ch>
2021-12-02 17:13:44 +0100

    Implemented Day 2

--- day02.py
+++ day02.py
@@ -2,11 +2,47 @@ from aocd import get_data, submit
 
 
 def part_a(data):
-    return ''
+    lines = data.split('\n')
+    print(len(lines))
+
+    hori = 0
+    depth = 0
+
+    for line in lines:
+        match line.split(" "):
+            case ["forward", x]:
+                hori += int(x)
+            case ["down", x]:
+                depth += int(x)
+            case ["up", x]:
+                depth -= int(x)
+            case _:
+                print(f"received unknown line {line}")
+
+    return str(hori * depth)
 
 
 def part_b(data):
-    return ''
+    lines = data.split('\n')
+    print(len(lines))
+
+    aim = 0
+    hori = 0
+    depth = 0
+    for line in lines:
+        match line.split(" "):
+            case ["forward", x]:
+                hori += int(x)
+                depth += (aim * int(x))
+            case ["down", x]:
+                aim += int(x)
+            case ["up", x]:
+                aim -= int(x)
+            case _:
+                print(f"received unknown line {line}")
+
+    return str(hori * depth)
+
 
 def main():
     data = get_data()
