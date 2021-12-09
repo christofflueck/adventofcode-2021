eb759fffa571d84b61aa1af1e9fb9ed178d5070a
Christof Flück <christof.flueck@comfone.com>
2021-12-09 10:34:57 +0100

    Refactored Day 9

--- day09.py
+++ day09.py
@@ -1,76 +1,77 @@
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Set
 
 from aocd import get_data, submit
 
 
-def parse_data(data: str) -> Tuple[Dict[Tuple[int, int], int], int, int]:
+def parse_data(data: str) -> Dict[Tuple[int, int], int]:
     parsed_data = {}
     lines = [[int(num) for num in line] for line in data.split('\n')]
-    max_y = len(lines)
-    max_x = len(lines[0])
     for y, line in enumerate(lines):
         for x, num in enumerate(line):
             parsed_data[(x, y)] = num
-    return parsed_data, max_x, max_y
+    return parsed_data
 
 
 def part_a(data):
-    height_map, max_x, max_y = parse_data(data)
+    height_map = parse_data(data)
     low_points = []
 
-    for x in range(max_x):
-        for y in range(max_y):
-            val = height_map[x, y]
-            if y > 0 and val >= height_map[x, y - 1]:
-                continue
-            if y < max_y - 1 and val >= height_map[x, y + 1]:
-                continue
-            if x > 0 and val >= height_map[x - 1, y]:
-                continue
-            if x < max_x - 1 and val >= height_map[x + 1, y]:
-                continue
-            low_points.append((x, y))
+    for point in height_map.keys():
+        val = height_map[point]
+        x = point[0]
+        y = point[1]
+        if (x, y - 1) in height_map and val >= height_map[x, y - 1]:
+            continue
+        if (x, y + 1) in height_map and val >= height_map[x, y + 1]:
+            continue
+        if (x - 1, y) in height_map and val >= height_map[x - 1, y]:
+            continue
+        if (x + 1, y) in height_map and val >= height_map[x + 1, y]:
+            continue
+        low_points.append((x, y))
     return sum([height_map[point] + 1 for point in low_points])
 
 
-def _get_neighbor(height_map: Dict[Tuple[int, int], int], coords: Tuple[int, int], neighbors: List[Tuple[int, int]],
-                  basins):
-    if coords in height_map and coords not in basins and height_map[coords] != 9:
-        neighbors.append(coords)
+def is_valid_neighbor(height_map: Dict[Tuple[int, int], int], coords: Tuple[int, int], basins) -> bool:
+    return coords in height_map and coords not in basins and height_map[coords] != 9
 
 
-def get_neighbors(height_map: Dict[Tuple[int, int], int], coords: Tuple[int, int], basins) -> List[Tuple[int, int]]:
-    neighbors = []
+def get_neighbors(height_map: Dict[Tuple[int, int], int], coords: Tuple[int, int], basins) -> Set[Tuple[int, int]]:
+    neighbors = set()
     x = coords[0]
     y = coords[1]
-    _get_neighbor(height_map, (x - 1, y), neighbors, basins)
-    _get_neighbor(height_map, (x + 1, y), neighbors, basins)
-    _get_neighbor(height_map, (x, y - 1), neighbors, basins)
-    _get_neighbor(height_map, (x, y + 1), neighbors, basins)
+
+    if is_valid_neighbor(height_map, (x - 1, y), basins):
+        neighbors.add((x - 1, y))
+    if is_valid_neighbor(height_map, (x + 1, y), basins):
+        neighbors.add((x + 1, y))
+    if is_valid_neighbor(height_map, (x, y - 1), basins):
+        neighbors.add((x, y - 1))
+    if is_valid_neighbor(height_map, (x, y + 1), basins):
+        neighbors.add((x, y + 1))
 
     return neighbors
 
 
 def part_b(data) -> int:
-    height_map, max_x, max_y = parse_data(data)
-    basins: List[Tuple[int, int]] = []
+    height_map = parse_data(data)
+    basins: Set[Tuple[int, int]] = set()
     all_basin_sizes = []
-    for x in range(max_x):
-        for y in range(max_y):
-            val = height_map[x, y]
-            if val == 9 or (x, y) in basins:
-                continue
-            current_basin = 0
-            neighbors = get_neighbors(height_map, (x, y), basins)
-            while len(neighbors) > 0:
-                curr_neighbors = neighbors
-                for neighbor in curr_neighbors:
-                    neighbors.extend(get_neighbors(height_map, neighbor, basins))
-                    neighbors.remove(neighbor)
-                    if neighbor not in basins:
-                        basins.append(neighbor)
-                        current_basin += 1
-            all_basin_sizes.append(current_basin)
+    for point in height_map.keys():
+        val = height_map[point]
+        if val == 9 or point in basins:
+            continue
+        current_basin = 0
+        neighbors = get_neighbors(height_map, point, basins)
+        while neighbors:
+            curr_neighbors = set(neighbors)
+            for neighbor in curr_neighbors:
+                neighbors.update(get_neighbors(height_map, neighbor, basins))
+                neighbors.remove(neighbor)
+                if neighbor not in basins:
+                    basins.add(neighbor)
+                    current_basin += 1
+        all_basin_sizes.append(current_basin)
     all_basin_sizes = sorted(all_basin_sizes)
     return all_basin_sizes[-1] * all_basin_sizes[-2] * all_basin_sizes[-3]
 
